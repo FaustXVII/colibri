fb6bf8a7a227fcb89b0dc0b1779b805289c2eea6
faust <industrialfaustxvii@gmail.com>
2018-02-26 20:36:58 +0100

    Banner new line fix, clear console option added

--- src/net/overload/Main.java
+++ src/net/overload/Main.java
@@ -1,5 +1,6 @@
 package net.overload;
 
+import net.overload.inputOutput.commandLine.TextOutput;
 import net.overload.inputOutput.hud.Banner;
 import net.overload.inputOutput.hud.MessageBox;
 import net.overload.inputOutput.StyledString;
@@ -9,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         Banner.printBanner();
 
-        new MessageBox(
+        new MessageBox(// write a generator example "Start\n<red>name</red>\n"+ sword +" more more"
                 new StyledString().white("Start"),
                 new StyledString().white("Load"),
                 new StyledString().white("Tutorial"),
@@ -17,5 +18,8 @@ public class Main {
                 new StyledString().white("Quit"),
                 new StyledString().interactive(new InteractiveObject("Sword", "S"))
         );
+
+        TextOutput.clearConsole();
+        Banner.printBanner();
     }
 }
--- src/net/overload/inputOutput/commandLine/TextOutput.java
+++ src/net/overload/inputOutput/commandLine/TextOutput.java
@@ -1,7 +1,15 @@
 package net.overload.inputOutput.commandLine;
 
 public class TextOutput {
-    public static void print(final String printMessage){
+    public static void print(final String printMessage) {
         System.out.print(printMessage);
+    }
+
+    public static void clearConsole() {
+        final String ANSI_CLS = "\u001b[2J";
+        final String ANSI_HOME = "\u001b[H";
+
+        System.out.print(ANSI_CLS + ANSI_HOME);
+        System.out.flush();
     }
 }
